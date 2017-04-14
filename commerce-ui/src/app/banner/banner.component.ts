e15e040d60c7af8343d9e2e65c6d3ea9d5d383a7
abhibasso <abhirup6@hotmail.com>
2017-04-14 16:38:18 +0200

    adding banner panel

---
+++ commerce-ui/src/app/banner/banner.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit, NgModule} from '@angular/core';
+
+
+@Component({
+  selector: 'app-bannercomponent',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.css']
+})
+export class BannerComponent implements OnInit {
+
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
