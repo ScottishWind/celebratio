f004dc716784efd32c827153f9746fab7a8eb578
Argha Banerjee <gabbu@arghas-mbp.home>
2017-04-30 12:45:27 +0100

    updating swagger comments

--- commerce-service/src/main/java/com/xcommerce/online/product/resource/ProductResource.java
+++ commerce-service/src/main/java/com/xcommerce/online/product/resource/ProductResource.java
@@ -6,6 +6,9 @@ import org.hibernate.validator.constraints.NotEmpty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,27 +28,15 @@ public class ProductResource {
 	@Autowired
 	private ProductFacade productFacade;
 
-	/**
-	 * @ApiOperation(value = "Insert Product Details - Old version", nickname =
-	 *                     "Product Insert")
-	 * @RequestMapping(value = "/create", method = RequestMethod.POST, consumes
-	 *                       = "application/json", produces =
-	 *                       "application/json") ProductBean
-	 *                       createProduct(@RequestBody ProductBean product) {
-	 *                       logger.info(String.format("New Product add request
-	 *                       submitted %S", product)); ProductBean newProduct =
-	 *                       productFacade.createProduct(product); return
-	 *                       newProduct; }
-	 **/
-
-	@ApiOperation(value = "Insert Product Details - Accepts list of items", nickname = "New Product Insert")
-	@RequestMapping(value = "/create", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	void createNewProduct(@RequestBody List<NewProductBean> product) {
+	@ApiOperation(value = "Inserts new Products - Accepts list of products", nickname = "New Product Insert")
+	@RequestMapping(value = "/create", method = RequestMethod.POST, consumes = "application/json")
+	ResponseEntity<?> createNewProduct(@RequestBody List<NewProductBean> product) {
 		logger.info(String.format("New Product add request submitted %S", product));
 		productFacade.createNewProduct(product);
+		return new ResponseEntity<>("success", HttpStatus.OK);
 	}
 
-	@ApiOperation(value = "Search Product By Category", nickname = "Category Search")
+	@ApiOperation(value = "Returns all Products that matches the Product Category", nickname = "Category Search")
 	@RequestMapping(value = "/search/category", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
 	List<NewProductBean> getProductDetailsBycategory(@RequestBody @NotEmpty String category) {
 		logger.info(String.format("Request to fetch Product details for category %S", category));
@@ -54,7 +45,7 @@ public class ProductResource {
 
 	}
 
-	@ApiOperation(value = "Search Product By Label", nickname = "Label Search")
+	@ApiOperation(value = "Returns all Products that matches the Label", nickname = "Label Search")
 	@RequestMapping(value = "/search/label", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
 	List<NewProductBean> getProductDetailsbyLabel(@RequestBody @NotEmpty String label) {
 		logger.info(String.format("Request to fetch Product details for label %S", label));
@@ -63,33 +54,36 @@ public class ProductResource {
 
 	}
 
-	@ApiOperation(value = "Homescreen products", nickname = "Category Search")
-	@RequestMapping(value = "/home", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+	@ApiOperation(value = "HomeScreen Products - **Currently returns all products**", nickname = "Category Search")
+	@RequestMapping(value = "/homescreen", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
 	List<NewProductBean> getAllProductHome() {
 		logger.info(String.format("Request to fetch Product details for homescreen"));
 		List<NewProductBean> productLst = productFacade.getHomeScreenProducts();
 		return productLst;
 
 	}
-	
-	@ApiOperation(value = "Delete Product by Product ID", nickname = "Delete Product")
-	@RequestMapping(value = "/delete/id", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	void deleteByProductId(@RequestBody @NotEmpty String productId) {
+
+	@ApiOperation(value = "Deletes a single Product that matches the input Product ID", nickname = "Delete Product")
+	@RequestMapping(value = "/delete/id", method = RequestMethod.POST, consumes = "application/json")
+	ResponseEntity<?> deleteByProductId(@RequestBody @NotEmpty String productId) {
 		logger.info(String.format("Request to delete Product by product ID %S", productId));
 		productFacade.deleteByProductId(productId);
+		return (new ResponseEntity<>("success", HttpStatus.OK));
 	}
-	
-	@ApiOperation(value = "Delete All Products in the Category", nickname = "Delete Product")
-	@RequestMapping(value = "/delete/category", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	void deleteByCategory(@RequestBody @NotEmpty String category) {
+
+	@ApiOperation(value = "Deletes All Products that belong to the input Category", nickname = "Delete Product")
+	@RequestMapping(value = "/delete/category", method = RequestMethod.POST, consumes = "application/json")
+	ResponseEntity<?> deleteByCategory(@RequestBody @NotEmpty String category) {
 		logger.info(String.format("Request to delete Product by category %S", category));
 		productFacade.deleteByCategory(category);
+		return (new ResponseEntity<>("success", HttpStatus.OK));
 	}
-	
-	@ApiOperation(value = "Delete All Product - **Risky Operation - Emties entire Database**", nickname = "Delete All Products")
-	@RequestMapping(value = "/delete/all", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	void deleteAll() {
+
+	@ApiOperation(value = "Deletes entire Product list from Backend - **Risky Operation**", nickname = "Delete All Products")
+	@RequestMapping(value = "/delete/all", method = RequestMethod.POST, consumes = "application/json")
+	ResponseEntity<?> deleteAll() {
 		logger.info("Request to delete All Products from backend Database");
 		productFacade.deleteAll();
+		return (new ResponseEntity<>("success", HttpStatus.OK));
 	}
 }
