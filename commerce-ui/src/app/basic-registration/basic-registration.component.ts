4c1f7a45b6f2abe351e8be8861e65484e593023f
abhibasso <abhirup6@hotmail.com>
2017-06-11 20:14:39 +0200

    adding basic url registration form

---
+++ commerce-ui/src/app/basic-registration/basic-registration.component.ts
@@ -0,0 +1,47 @@
+import {Component, OnInit} from '@angular/core';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {AbstractControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-basic-registration',
+  templateUrl: './basic-registration.component.html',
+  styleUrls: ['./basic-registration.component.css']
+})
+export class BasicRegistrationComponent {
+
+  complexForm: FormGroup;
+  imageLocation = "";
+
+  constructor(public activeModal: NgbActiveModal, private fb: FormBuilder) {
+    this.complexForm = fb.group({
+      'profilePic': [null, Validators.compose([Validators.required])],
+      'email': [null, Validators.compose([Validators.required, Validators.email])],
+      'password': [null, Validators.compose([Validators.required, Validators.minLength(5)])],
+      'rePassword': [null, Validators.compose([Validators.required])]
+    }, {
+      validator: PasswordValidation.matchPassword
+    });
+  }
+
+
+  submitForm(value: any) {
+    console.log(value);
+  }
+}
+
+class PasswordValidation {
+  static matchPassword(complexForm: FormGroup) {
+    if (complexForm) {
+      let password = complexForm.controls.password.value;
+      let confirmPassword = complexForm.controls.rePassword.value;
+      if (password != confirmPassword) {
+        console.log('false');
+        complexForm.controls.rePassword.setErrors({MatchPassword: true})
+      } else {
+        console.log('true');
+        return null
+      }
+    }
+  }
+
+}
---
+++ commerce-ui/src/app/services/registrationService.ts
@@ -0,0 +1,50 @@
+import {Injectable}              from '@angular/core';
+import {Product} from '../products/model/product';
+import {Observable} from 'rxjs/Observable';
+import {Subject} from 'rxjs/Subject';
+import {factoryOrValue} from "rxjs/operator/multicast";
+import 'rxjs/add/observable/of';
+
+
+@Injectable()
+export class CartService {
+  product$: Subject<Product> = new Subject<Product>();
+  productList: Product[] = new Array<Product>();
+
+  constructor() {
+    this.product$.subscribe(product => {
+      this.productList.push(product);
+    });
+  }
+
+
+  addToCart(product: Product) {
+    this.product$.next(product);
+  }
+
+  getProductList(): Observable<Product[]> {
+    return Observable.of(this.productList);
+  }
+
+  checkExistsInCart(checkProduct: Product): boolean {
+    let exists = false;
+    this.productList.forEach(product => {
+      if (product.productID === checkProduct.productID) {
+        exists = true;
+      }
+    });
+    return exists;
+  }
+
+  removeFromCart(checkProduct: Product) {
+    var itemIndex;
+    this.productList.forEach((product, index) => {
+      if (product.productID === checkProduct.productID) {
+        itemIndex = index;
+      }
+    });
+    this.productList.splice(itemIndex, 1);
+  }
+
+
+}
