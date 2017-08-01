40a5c43c566fbbac5e0985563264abb5ce4d225e
Argha Banerjee <gabbu@Arghas-MacBook-Pro.local>
2017-08-01 17:10:39 +0100

    config changes

---
+++ commerce-micro-services/commerce-config-server/src/main/java/com/commerce/online/commerceconfigserver/CommerceConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.commerce.online.commerceconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceConfigServerApplication.class, args);
+	}
+}
