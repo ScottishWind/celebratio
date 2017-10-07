ba7bfcff5964dc06d8112bb5b9d853d28bf32557
saurabh <skakliya@gmail.com>
2017-10-07 19:39:47 +0530

    order summary form

--- commerce-ui/src/app/order/order.component.ts
+++ commerce-ui/src/app/order/order.component.ts
@@ -1,15 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+/*import {CartService} from '../services/cartService';*/
+import {Product} from '../products/model/product';
+import {Subscription} from 'rxjs/Subscription';
+import {CartService} from '../services/cartService';
+import {Router} from '@angular/router';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css']
 })
-export class OrderComponent implements OnInit {
+export class OrderComponent {
+	sum = 0;
+  productList: Product[] = new Array<Product>();
+  complexForm: FormGroup;
+  cities: String[] = new Array<String>();
+  states: String[] = new Array<String>();
+  countries: String[] = new Array<String>();
+  
 
-  constructor() { }
+   constructor(private cartService: CartService, private router: Router, private fb: FormBuilder) {
+    this.cartService.getProductList().subscribe(productList => {
+    	console.log(productList);
+      this.productList = productList;
+    });
 
-  ngOnInit() {
+     this.complexForm = fb.group({
+      'firstName': [null, Validators.required],
+      'lastName': [null, Validators.required],
+      'email': [null, Validators.compose([Validators.required, Validators.email])],
+      'phoneNumber': [null],
+      'password': [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
+      'firstLine': [null, Validators.required],
+      'secondLine': [null],
+      'thirdLine': [null],
+      'landmark': [null],
+      'postcode': [null, Validators.required],
+      'city': [null, Validators.required],
+      'state': [null, Validators.required],
+      'country': [null, Validators.required]
+    });
+    this.populateCities();
+    this.populateStates();
+    this.populateCountry();
   }
 
+
+  
+
+ openAccordian(formPanel,ele){
+    document.getElementById(ele).classList.toggle("active");
+    let panel:any = document.getElementById(formPanel);
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+
+   populateCities() {
+    this.cities.push("Kolkata", "Mumbai", "Delhi", "Bangalore");
+  }
+
+  populateStates() {
+    this.states.push("West Bengal", "Maharashtra", "Karnataka", "Uttar Pradesh");
+  }
+
+  populateCountry() {
+    this.countries.push("India");
+  }
+
+	submitForm(value: any) {
+	   this.openAccordian('orderPanel','orderSummary')
+	  }
+
 }
+
+
